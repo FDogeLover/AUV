8416d83dbde77b3b3d46747c8207984faa4acb6d
“DogeLover” <3011168254@qq.com>
2026-05-19 20:49:19 +0800

    项目重组：源代码集中到 Project2/ 子目录
    将 ANO_LX_FC_倾角保护版、drone_control、CodeWiki、k230 等源码目录
    从仓库根目录迁移到 Project2/ 子目录下统一管理

--- ANO_LX_FC_倾角保护版/Mycode/angle_protect.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ANGLE_PROTECT_H
-#define _ANGLE_PROTECT_H
-
-#include "ANO_LX.h"
-//#include   // ±ØÐë°üº¬
-
-u8 Attitude_Check(void);
-
-#endif
--- drone_control/Lcode/global_variable.py
+++
@@ -1,7 +0,0 @@
-import threading
-import time
-from multiprocessing import Value
-sp_side=51 #串口传输速度偏置量
-lock=threading.Lock()#线程锁
-task_start_sign=Value("b",False)
-fc_last_rx_time = 0.0  # 飞控串口最后收到有效帧的时间戳
